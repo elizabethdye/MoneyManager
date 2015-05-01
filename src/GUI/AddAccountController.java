3e997ce8aec8802ffba5f98ad0c72f95f0d16499
elizabethdye <dyeea@hendrix.edu>
2015-05-01 10:13:43 -0500

    Working on Error Message Enums
    So we don’t have to copy the same thing over and over, just call a
    method with an enum

--- src/GUI/AddAccountController.java
+++ src/GUI/AddAccountController.java
@@ -1,14 +1,20 @@
 package GUI;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import Model.DatabaseCommand;
+import Model.ServerRequest;
+import Networking.Networker;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-/**
- * Created by My on 4/26/2015.
- */
 public class AddAccountController {
-
+	Networker net;
+	String ID;
+	
     private boolean commited = true;
     private Stage stage;
 
@@ -24,13 +30,21 @@ public class AddAccountController {
     public boolean isCommited() {
         return this.commited;
     }
-
+   
     @FXML
     public void handleCommit()throws NumberFormatException{
         String name = accNameField.getText();
         Double balance = Double.valueOf(balanceField.getText());
-
-        //TODO: call method to save the data to the database
+        
+        DatabaseCommand cmd1 = DatabaseCommand.ADD_ACCT;
+		String[] args1 = {ID, name};
+		ServerRequest request1 = new ServerRequest(cmd1, args1);
+		net.sendServerRequest(request1);
+		
+		DatabaseCommand cmd2 = DatabaseCommand.CREATE_DEPOSIT;
+		String[] args2 = {ID, name, Double.toString(balance), getDate()};
+		ServerRequest request2 = new ServerRequest(cmd2, args2);
+		net.sendServerRequest(request2);
 
         commited = true;
         stage.close();
@@ -38,4 +52,15 @@ public class AddAccountController {
 
     @FXML
     public void handleCancel(){ stage.close();}
+    
+    public void setUser(String ID){
+    	this.ID = ID;
+    	net = new Networker();
+    }
+    
+    public String getDate(){
+    	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    	Date date = new Date();
+ 	   	return dateFormat.format(date);
+    }
 }
