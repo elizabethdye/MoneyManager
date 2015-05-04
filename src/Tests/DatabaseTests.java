1c59a7ba52a143a4918c76e4fab1e4eb2337a827
ra-perez <PerezRA@hendrix.edu>
2015-05-03 21:39:40 -0500

    Attempt at fixing database methods

--- src/Tests/DatabaseTests.java
+++ src/Tests/DatabaseTests.java
@@ -3,6 +3,7 @@ package Tests;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -11,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import Database.Database;
+import Model.TType;
 import Model.UserType;
 
 
@@ -87,6 +89,20 @@ public class DatabaseTests {
 		assertEquals((Double)300.0, db.getBalance("User1", "Savings"));
 	}
 	
+	@Test
+	public void testGetCategories() throws ClassNotFoundException, SQLException {
+		addTwoWithdrawals();
+		ArrayList<String> checkingCategories = db.getCategories("User1", "Checking");
+		ArrayList<String> savingsCategories = db.getCategories("User1", "Savings");
+		assertEquals(checkingCategories.size(), 1);
+		assertEquals(savingsCategories.size(), 1);
+		assertEquals(checkingCategories.get(0), "Groceries");
+		assertEquals(savingsCategories.get(0), "Rent");
+
+
+		
+	}
+	
 	public void addTwoDeposits() throws SQLException, ClassNotFoundException {
 		db.createDeposit("User1", "Checking", 125.0, "March 26");
 		db.createDeposit("User1", "Savings", 250.0, "April 27");
@@ -102,4 +118,38 @@ public class DatabaseTests {
 		db.createTransfer("User1", "Savings", "Checking", 50.0, "April 1");
 	}
 	
+	
+//	public ArrayList<String> getCategories(String name, String account) throws ClassNotFoundException, SQLException {
+//		String query = "SELECT Category FROM Transactions WHERE UserID = '" + name + "' AND WHERE Account = '" + account + "'";
+//		return makeQuery(query);
+//	}
+//	
+//	public ArrayList<Double> getTransactionsForCategory(String name, String account, String category) throws ClassNotFoundException, SQLException {
+//		String query = "SELECT Amount FROM Transactions WHERE UserID = '" + name + "' AND WHERE Account = '" + account + "' AND WHERE Category = '" + category + "'";
+//		ArrayList<String> result = makeQuery(query);
+//		ArrayList<Double> toReturn = new ArrayList<Double>();
+//		for (String s: result) {
+//			toReturn.add(Double.parseDouble(s));
+//		}
+//		return toReturn;
+//	}
+//	
+//	public ArrayList<String> getTransactions(String name, String account) throws SQLException {
+//		String query = "SELECT TransactionType, Amount, Category, Date FROM Transactions WHERE UserID = '" + name + "' AND WHERE Account = '" + account + "' AND WHERE TransactionType <> '" + TType.DEPOSIT.toString() +"' ORDER BY Date";
+//        ResultSet rs = stat.executeQuery(query);
+//        String separator = "~;~";
+//		ArrayList<String> toReturn = new ArrayList<String>();
+//        while (rs.next()) {
+//        	String type = rs.getString("TransactionType");
+//        	Double amount = rs.getDouble("Amount");
+//        	String category = rs.getString("Category");
+//        	String date = rs.getString("Date");
+//        	String transactionVal = type + separator +
+//        			amount.toString() + separator +
+//        			category + separator +
+//        			date;
+//        	toReturn.add(transactionVal);
+//        }
+//		return toReturn;
+//	}
 }
